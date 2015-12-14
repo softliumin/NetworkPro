bdb83435a0000e64aba1c9aadcca6fe60066c999
sharper <softliumin@gmail.com>
2015-12-14 19:53:44 +0800

    :joy:

--- src/main/java/cc/sharper/Socket/MultithreadedDaytimeServer.java
+++ src/main/java/cc/sharper/Socket/MultithreadedDaytimeServer.java
@@ -7,46 +7,60 @@ import java.util.Date;
 /**
  * Created by liumin3 on 2015/12/14.
  */
-public class MultithreadedDaytimeServer {
+public class MultithreadedDaytimeServer
+{
     public final static int PORT = 13;
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
 
-        try {
+        try
+        {
             ServerSocket server = new ServerSocket(PORT);
-            while (true) {
-                try {
+            while (true)
+            {
+                try
+                {
                     Socket connection = server.accept();
                     Thread task = new DaytimeThread(connection);
                     task.start();
                 } catch (IOException ex) {}
             }
-        } catch (IOException ex) {
+        } catch (IOException ex)
+        {
             System.err.println("Couldn't start server");
         }
     }
 
-    private static class DaytimeThread extends Thread {
+    private static class DaytimeThread extends Thread
+    {
 
         private Socket connection;
 
-        DaytimeThread(Socket connection) {
+        DaytimeThread(Socket connection)
+        {
             this.connection = connection;
         }
 
         @Override
-        public void run() {
-            try {
+        public void run()
+        {
+            try
+            {
                 Writer out = new OutputStreamWriter(connection.getOutputStream());
                 Date now = new Date();
                 out.write(now.toString() +"\r\n");
                 out.flush();
-            } catch (IOException ex) {
+            } catch (IOException ex)
+            {
                 System.err.println(ex);
-            } finally {
-                try {
+            } finally
+            {
+                try
+                {
                     connection.close();
-                } catch (IOException e) {
+                } catch (IOException e)
+                {
                     // ignore;
                 }
             }
--- src/main/java/cc/sharper/Socket/PooledDaytimeServer.java
+++ src/main/java/cc/sharper/Socket/PooledDaytimeServer.java
@@ -7,48 +7,60 @@ import java.util.concurrent.*;
 /**
  * Created by liumin3 on 2015/12/14.
  */
-public class PooledDaytimeServer {
+public class PooledDaytimeServer
+{
     public final static int PORT = 13;
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
 
         ExecutorService pool = Executors.newFixedThreadPool(50);
 
-        try  {
+        try
+        {
             ServerSocket server = new ServerSocket(PORT);
-            while (true) {
-                try {
+            while (true)
+            {
+                try
+                {
                     Socket connection = server.accept();
                     Callable<Void> task = new DaytimeTask(connection);
                     pool.submit(task);
                 } catch (IOException ex) {}
             }
-        } catch (IOException ex) {
+        } catch (IOException ex)
+        {
             System.err.println("Couldn't start server");
         }
     }
 
-    private static class DaytimeTask implements Callable<Void> {
+    private static class DaytimeTask implements Callable<Void>
+    {
 
         private Socket connection;
-
-        DaytimeTask(Socket connection) {
+        DaytimeTask(Socket connection)
+        {
             this.connection = connection;
         }
-
         //@Override
-        public Void call() {
-            try {
+        public Void call()
+        {
+            try
+            {
                 Writer out = new OutputStreamWriter(connection.getOutputStream());
                 Date now = new Date();
                 out.write(now.toString() +"\r\n");
                 out.flush();
-            } catch (IOException ex) {
+            } catch (IOException ex)
+            {
                 System.err.println(ex);
-            } finally {
-                try {
+            } finally
+            {
+                try
+                {
                     connection.close();
-                } catch (IOException e) {
+                } catch (IOException e)
+                {
                     // ignore;
                 }
             }
