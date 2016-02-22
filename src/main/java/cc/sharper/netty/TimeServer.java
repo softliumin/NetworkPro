5bd8a450dba9eda89f2e8928e859ce7e6d8f1abb
sharper <softliumin@gmail.com>
2016-02-22 16:48:29 +0800

    :joy:整理代码

--- src/main/java/cc/sharper/netty/TimeServer.java
+++ src/main/java/cc/sharper/netty/TimeServer.java
@@ -26,38 +26,44 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
 
 /**
  * @author lilinfeng
- * @date 2014年2月14日
  * @version 1.0
+ * @date 2014年2月14日
  */
-public class TimeServer {
+public class TimeServer
+{
 
-    public void bind(int port) throws Exception {
-	// 配置服务端的NIO线程组
-	EventLoopGroup bossGroup = new NioEventLoopGroup();
-	EventLoopGroup workerGroup = new NioEventLoopGroup();
-	try {
-	    ServerBootstrap b = new ServerBootstrap();
-	    b.group(bossGroup, workerGroup)
-		    .channel(NioServerSocketChannel.class)
-		    .option(ChannelOption.SO_BACKLOG, 1024)
-		    .childHandler(new ChildChannelHandler());
-	    // 绑定端口，同步等待成功
-	    ChannelFuture f = b.bind(port).sync();
+    public void bind(int port) throws Exception
+    {
+        // 配置服务端的NIO线程组 一个进行网络连接，一个进行SocketChannel的网络读写
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try
+        {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 1024)
+                    .childHandler(new ChildChannelHandler());//绑定io的事件
+            // 绑定端口，同步等待成功
+            ChannelFuture f = b.bind(port).sync();
 
-	    // 等待服务端监听端口关闭
-	    f.channel().closeFuture().sync();
-	} finally {
-	    // 优雅退出，释放线程池资源
-	    bossGroup.shutdownGracefully();
-	    workerGroup.shutdownGracefully();
-	}
+            // 等待服务端监听端口关闭
+            f.channel().closeFuture().sync();
+        } finally
+        {
+            // 优雅退出，释放线程池资源
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
     }
 
-    private class ChildChannelHandler extends ChannelInitializer<SocketChannel> {
-	@Override
-	protected void initChannel(SocketChannel arg0) throws Exception {
-	    arg0.pipeline().addLast(new TimeServerHandler());
-	}
+    private class ChildChannelHandler extends ChannelInitializer<SocketChannel>
+    {
+        @Override
+        protected void initChannel(SocketChannel arg0) throws Exception
+        {
+            arg0.pipeline().addLast(new TimeServerHandler());
+        }
 
     }
 
@@ -65,15 +71,19 @@ public class TimeServer {
      * @param args
      * @throws Exception
      */
-    public static void main(String[] args) throws Exception {
-	int port = 8080;
-	if (args != null && args.length > 0) {
-	    try {
-		port = Integer.valueOf(args[0]);
-	    } catch (NumberFormatException e) {
-		// 采用默认值
-	    }
-	}
-	new TimeServer().bind(port);
+    public static void main(String[] args) throws Exception
+    {
+        int port = 8080;
+        if (args != null && args.length > 0)
+        {
+            try
+            {
+                port = Integer.valueOf(args[0]);
+            } catch (NumberFormatException e)
+            {
+                // 采用默认值
+            }
+        }
+        new TimeServer().bind(port);
     }
 }
