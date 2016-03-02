826a549272922946bd4cb33aaab09b78d14fba95
sharper <softliumin@gmail.com>
2016-03-02 11:27:51 +0800

    :joy:学习封装自己私有协议栈

---
+++ src/main/java/cc/sharper/protocol/client/LoginAuthReqHandler.java
@@ -0,0 +1,75 @@
+package cc.sharper.protocol.client;
+
+import cc.sharper.protocol.MessageType;
+import cc.sharper.protocol.struct.Header;
+import cc.sharper.protocol.struct.NettyMessage;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelPipeline;
+
+
+/**
+ * 握手认证客户端，通道激活时，发送握手请求
+ */
+public class LoginAuthReqHandler extends ChannelHandlerAdapter
+{
+
+    /**
+     * Calls {@link ChannelHandlerContext#fireChannelActive()} to forward to the
+     * next {@link ChannelHandler} in the {@link ChannelPipeline}.
+     * <p/>
+     * Sub-classes may override this method to change behavior.
+     */
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception
+    {
+        ctx.writeAndFlush(buildLoginReq());
+    }
+
+    /** 对握手返回信息进行处理
+     * Calls {@link ChannelHandlerContext#fireChannelRead(Object)} to forward to
+     * the next {@link ChannelHandler} in the {@link ChannelPipeline}.
+     * <p/>
+     * Sub-classes may override this method to change behavior.
+     */
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg)
+            throws Exception
+    {
+        NettyMessage message = (NettyMessage) msg;
+
+        // 如果是握手应答消息，需要判断是否认证成功
+        if (message.getHeader() != null
+                && message.getHeader().getType() == MessageType.LOGIN_RESP
+                .value())
+        {
+            byte loginResult = (byte) message.getBody();
+            if (loginResult != (byte) 0)
+            {
+                // 握手失败，关闭连接
+                ctx.close();
+            } else
+            {
+                System.out.println("Login is ok : " + message);
+                ctx.fireChannelRead(msg);
+            }
+        } else
+            ctx.fireChannelRead(msg);
+    }
+
+    private NettyMessage buildLoginReq()
+    {
+        NettyMessage message = new NettyMessage();
+        Header header = new Header();
+        header.setType(MessageType.LOGIN_REQ.value());
+        message.setHeader(header);
+        return message;
+    }
+
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause)
+            throws Exception
+    {
+        ctx.fireExceptionCaught(cause);
+    }
+}
---
+++ src/main/java/cc/sharper/protocol/test/TestProtocol.java
@@ -0,0 +1,12 @@
+package cc.sharper.protocol.test;
+
+/**
+ * Created by liumin3 on 2016/3/2.
+ */
+public class TestProtocol
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
