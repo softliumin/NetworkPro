e98429649589d4e0d82027b5ba0b26c771a15694
sharper <softliumin@gmail.com>
2016-03-05 14:18:37 +0800

    :joy:自己的一个聊天工具，利用netty

--- src/main/java/cc/sharper/mysimplechat/ChatServer.java
+++ src/main/java/cc/sharper/mysimplechat/ChatServer.java
@@ -1,13 +1,16 @@
 package cc.sharper.mysimplechat;
 
 import cc.sharper.simplechat.SimpleChatClientInitializer;
+import cc.sharper.simplechat.SimpleChatServerHandler;
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelOption;
-import io.netty.channel.EventLoopGroup;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.Delimiters;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
 
 /**
  * Created by liumin3 on 2016/3/4.
@@ -32,7 +35,20 @@ public class ChatServer
             ServerBootstrap b = new  ServerBootstrap();
             b.group(bossGroup,workerGroup)
                     .channel(NioServerSocketChannel.class)
-                    .childHandler(new SimpleChatClientInitializer())
+                    .childHandler(new  ChannelInitializer(){
+                        @Override
+                        protected void initChannel(Channel ch) throws Exception
+                        {
+                            ChannelPipeline pipeline = ch.pipeline();
+
+                            pipeline.addLast("framer", new DelimiterBasedFrameDecoder(8192, Delimiters.lineDelimiter()));
+                            pipeline.addLast("decoder", new StringDecoder());
+                            pipeline.addLast("encoder", new StringEncoder());
+                            pipeline.addLast("handler", new ChatServerHandler());
+
+                            System.out.println("聊天客户端:" + ch.remoteAddress() + "连接上");
+                        }
+                    })
                     .option(ChannelOption.SO_BACKLOG,128)
                     .childOption(ChannelOption.SO_KEEPALIVE,true);
 
--- src/main/java/cc/sharper/mysimplechat/ChatServerHandler.java
+++ src/main/java/cc/sharper/mysimplechat/ChatServerHandler.java
@@ -1,8 +1,79 @@
 package cc.sharper.mysimplechat;
 
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.util.concurrent.GlobalEventExecutor;
+
 /**
  * Created by liumin3 on 2016/3/4.
  */
-public class ChatServerHandler
+public class ChatServerHandler extends SimpleChannelInboundHandler<String>
 {
+
+    private static ChannelGroup channels = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+
+    @Override
+    protected void messageReceived(ChannelHandlerContext ctx, String msg) throws Exception
+    {
+        Channel incoming = ctx.channel();
+
+        for (Channel channel : channels)
+        {
+            if (channel != incoming )
+            {
+                channel.writeAndFlush("["+incoming.remoteAddress()+"]" +msg +"\n");
+            }else
+            {
+                channel.writeAndFlush("[你]"+msg+ "\n");
+            }
+        }
+    }
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception
+    {
+        Channel incoming = ctx.channel();
+
+        channels.writeAndFlush("[服务器]"+incoming.remoteAddress()+"加入\n");
+
+        channels.add(ctx.channel());
+    }
+
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception
+    {
+        Channel incoming = ctx.channel();
+
+        channels.writeAndFlush("[服务器]"+incoming.remoteAddress()+"离开\n");
+
+        channels.remove(ctx.channel());
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception
+    {
+        Channel incoming = ctx.channel();
+        System.out.println("聊天客户端"+ incoming.remoteAddress() +"在线");
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception
+    {
+        Channel incoming = ctx.channel();
+        System.out.println("聊天客户端"+incoming.remoteAddress() +"掉线");
+    }
+
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception
+    {
+        Channel incoming = ctx.channel();
+        System.out.println("聊天客户端" + incoming.remoteAddress()+"异常");
+
+        cause.printStackTrace();
+        ctx.channel();
+    }
 }
